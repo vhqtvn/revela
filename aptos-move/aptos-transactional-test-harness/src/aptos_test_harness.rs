sgilflores3/aptos-core

trivial: StateView::get_state_value_bytes
in preparation for exposing StateValue

aptos-move/aptos-transactional-test-harness/src/aptos_test_harness.rs
@@ -368,7 +368,7 @@ impl<'a> AptosTestAdapter<'a> {
             AccessPath::resource_access_path(*signer_addr, AccountResource::struct_tag());
         let account_blob = self
             .storage
-            .get_state_value(&StateKey::AccessPath(account_access_path))
+            .get_state_value_bytes(&StateKey::AccessPath(account_access_path))
             .unwrap()
             .ok_or_else(|| {
                 format_err!(
@@ -388,7 +388,7 @@ impl<'a> AptosTestAdapter<'a> {
 
         let balance_blob = self
             .storage
-            .get_state_value(&StateKey::AccessPath(coin_access_path))
+            .get_state_value_bytes(&StateKey::AccessPath(coin_access_path))
             .unwrap()
             .ok_or_else(|| {
                 format_err!(
@@ -895,7 +895,7 @@ impl<'a> MoveTestAdapter<'a> for AptosTestAdapter<'a> {
 
                 let bytes = self
                     .storage
-                    .get_state_value(&state_key)
+                    .get_state_value_bytes(&state_key)
                     .unwrap()
                     .ok_or_else(|| format_err!("Failed to fetch table item.",))?;
 

aptos-move/aptos-vm/src/data_cache.rs
@@ -109,7 +109,7 @@ impl<'a, S: StateView> StorageAdapter<'a, S> {
 
     pub fn get(&self, access_path: AccessPath) -> PartialVMResult<Option<Vec<u8>>> {
         self.0
-            .get_state_value(&StateKey::AccessPath(access_path))
+            .get_state_value_bytes(&StateKey::AccessPath(access_path))
             .map_err(|_| PartialVMError::new(StatusCode::STORAGE_ERROR))
     }
 }
@@ -168,7 +168,7 @@ impl<'a, S: StateView> TableResolver for StorageAdapter<'a, S> {
         handle: &TableHandle,
         key: &[u8],
     ) -> Result<Option<Vec<u8>>, Error> {
-        self.get_state_value(&StateKey::table_item((*handle).into(), key.to_vec()))
+        self.get_state_value_bytes(&StateKey::table_item((*handle).into(), key.to_vec()))
     }
 }
 

aptos-move/e2e-tests/src/executor.rs
@@ -289,9 +289,10 @@ impl FakeExecutor {
 
     pub fn read_resource<T: MoveResource>(&self, addr: &AccountAddress) -> Option<T> {
         let ap = AccessPath::resource_access_path(*addr, T::struct_tag());
-        let data_blob = TStateView::get_state_value(&self.data_store, &StateKey::AccessPath(ap))
-            .expect("account must exist in data store")
-            .unwrap_or_else(|| panic!("Can't fetch {} resource for {}", T::STRUCT_NAME, addr));
+        let data_blob =
+            TStateView::get_state_value_bytes(&self.data_store, &StateKey::AccessPath(ap))
+                .expect("account must exist in data store")
+                .unwrap_or_else(|| panic!("Can't fetch {} resource for {}", T::STRUCT_NAME, addr));
         bcs::from_bytes(data_blob.as_slice()).ok()
     }
 
@@ -465,7 +466,7 @@ impl FakeExecutor {
 
     /// Get the blob for the associated AccessPath
     pub fn read_state_value(&self, state_key: &StateKey) -> Option<Vec<u8>> {
-        TStateView::get_state_value(&self.data_store, state_key).unwrap()
+        TStateView::get_state_value_bytes(&self.data_store, state_key).unwrap()
     }
 
     /// Set the blob for the associated AccessPath

execution/executor/src/db_bootstrapper.rs
@@ -197,7 +197,7 @@ pub fn calculate_genesis<V: VMExecutor>(
 
 fn get_state_timestamp(state_view: &CachedStateView) -> Result<u64> {
     let rsrc_bytes = &state_view
-        .get_state_value(&StateKey::AccessPath(AccessPath::new(
+        .get_state_value_bytes(&StateKey::AccessPath(AccessPath::new(
             CORE_CODE_ADDRESS,
             TimestampResource::resource_path(),
         )))?
@@ -208,7 +208,7 @@ fn get_state_timestamp(state_view: &CachedStateView) -> Result<u64> {
 
 fn get_state_epoch(state_view: &CachedStateView) -> Result<u64> {
     let rsrc_bytes = &state_view
-        .get_state_value(&StateKey::AccessPath(AccessPath::new(
+        .get_state_value_bytes(&StateKey::AccessPath(AccessPath::new(
             CORE_CODE_ADDRESS,
             ConfigurationResource::resource_path(),
         )))?

execution/executor/src/mock_vm/mod.rs
@@ -233,7 +233,7 @@ fn read_seqnum_from_storage(state_view: &impl StateView, seqnum_access_path: &Ac
 
 fn read_u64_from_storage(state_view: &impl StateView, access_path: &AccessPath) -> u64 {
     state_view
-        .get_state_value(&StateKey::AccessPath(access_path.clone()))
+        .get_state_value_bytes(&StateKey::AccessPath(access_path.clone()))
         .expect("Failed to query storage.")
         .map_or(0, |bytes| decode_bytes(&bytes))
 }
@@ -243,7 +243,7 @@ fn read_state_value_from_storage(
     access_path: &AccessPath,
 ) -> Option<Vec<u8>> {
     state_view
-        .get_state_value(&StateKey::AccessPath(access_path.clone()))
+        .get_state_value_bytes(&StateKey::AccessPath(access_path.clone()))
         .expect("Failed to query storage.")
 }
 

vhqtvn/revela

aptos-move/aptos-transactional-test-harness/src/aptos_test_harness.rs
@@ -368,7 +368,7 @@ impl<'a> AptosTestAdapter<'a> {
             AccessPath::resource_access_path(*signer_addr, AccountResource::struct_tag());
         let account_blob = self
             .storage
-            .get_state_value(&StateKey::access_path(account_access_path))
+            .get_state_value_bytes(&StateKey::access_path(account_access_path))
             .unwrap()
             .ok_or_else(|| {
                 format_err!(
@@ -388,7 +388,7 @@ impl<'a> AptosTestAdapter<'a> {
 
         let balance_blob = self
             .storage
-            .get_state_value(&StateKey::access_path(coin_access_path))
+            .get_state_value_bytes(&StateKey::access_path(coin_access_path))
             .unwrap()
             .ok_or_else(|| {
                 format_err!(
@@ -895,7 +895,7 @@ impl<'a> MoveTestAdapter<'a> for AptosTestAdapter<'a> {
 
                 let bytes = self
                     .storage
-                    .get_state_value(&state_key)
+                    .get_state_value_bytes(&state_key)
                     .unwrap()
                     .ok_or_else(|| format_err!("Failed to fetch table item.",))?;
 

aptos-move/aptos-vm/src/data_cache.rs
@@ -109,7 +109,7 @@ impl<'a, S: StateView> StorageAdapter<'a, S> {
 
     pub fn get(&self, access_path: AccessPath) -> PartialVMResult<Option<Vec<u8>>> {
         self.0
-            .get_state_value(&StateKey::access_path(access_path))
+            .get_state_value_bytes(&StateKey::access_path(access_path))
             .map_err(|_| PartialVMError::new(StatusCode::STORAGE_ERROR))
     }
 }
@@ -168,7 +168,7 @@ impl<'a, S: StateView> TableResolver for StorageAdapter<'a, S> {
         handle: &TableHandle,
         key: &[u8],
     ) -> Result<Option<Vec<u8>>, Error> {
-        self.get_state_value(&StateKey::table_item((*handle).into(), key.to_vec()))
+        self.get_state_value_bytes(&StateKey::table_item((*handle).into(), key.to_vec()))
     }
 }
 

aptos-move/e2e-tests/src/executor.rs
@@ -289,9 +289,10 @@ impl FakeExecutor {
 
     pub fn read_resource<T: MoveResource>(&self, addr: &AccountAddress) -> Option<T> {
         let ap = AccessPath::resource_access_path(*addr, T::struct_tag());
-        let data_blob = TStateView::get_state_value(&self.data_store, &StateKey::access_path(ap))
-            .expect("account must exist in data store")
-            .unwrap_or_else(|| panic!("Can't fetch {} resource for {}", T::STRUCT_NAME, addr));
+        let data_blob =
+            TStateView::get_state_value_bytes(&self.data_store, &StateKey::access_path(ap))
+                .expect("account must exist in data store")
+                .unwrap_or_else(|| panic!("Can't fetch {} resource for {}", T::STRUCT_NAME, addr));
         bcs::from_bytes(data_blob.as_slice()).ok()
     }
 
@@ -465,7 +466,7 @@ impl FakeExecutor {
 
     /// Get the blob for the associated AccessPath
     pub fn read_state_value(&self, state_key: &StateKey) -> Option<Vec<u8>> {
-        TStateView::get_state_value(&self.data_store, state_key).unwrap()
+        TStateView::get_state_value_bytes(&self.data_store, state_key).unwrap()
     }
 
     /// Set the blob for the associated AccessPath

execution/executor/src/db_bootstrapper.rs
@@ -197,7 +197,7 @@ pub fn calculate_genesis<V: VMExecutor>(
 
 fn get_state_timestamp(state_view: &CachedStateView) -> Result<u64> {
     let rsrc_bytes = &state_view
-        .get_state_value(&StateKey::access_path(AccessPath::new(
+        .get_state_value_bytes(&StateKey::access_path(AccessPath::new(
             CORE_CODE_ADDRESS,
             TimestampResource::resource_path(),
         )))?
@@ -208,7 +208,7 @@ fn get_state_timestamp(state_view: &CachedStateView) -> Result<u64> {
 
 fn get_state_epoch(state_view: &CachedStateView) -> Result<u64> {
     let rsrc_bytes = &state_view
-        .get_state_value(&StateKey::access_path(AccessPath::new(
+        .get_state_value_bytes(&StateKey::access_path(AccessPath::new(
             CORE_CODE_ADDRESS,
             ConfigurationResource::resource_path(),
         )))?

execution/executor/src/mock_vm/mod.rs
@@ -233,7 +233,7 @@ fn read_seqnum_from_storage(state_view: &impl StateView, seqnum_access_path: &Ac
 
 fn read_u64_from_storage(state_view: &impl StateView, access_path: &AccessPath) -> u64 {
     state_view
-        .get_state_value(&StateKey::access_path(access_path.clone()))
+        .get_state_value_bytes(&StateKey::access_path(access_path.clone()))
         .expect("Failed to query storage.")
         .map_or(0, |bytes| decode_bytes(&bytes))
 }
@@ -243,7 +243,7 @@ fn read_state_value_from_storage(
     access_path: &AccessPath,
 ) -> Option<Vec<u8>> {
     state_view
-        .get_state_value(&StateKey::access_path(access_path.clone()))
+        .get_state_value_bytes(&StateKey::access_path(access_path.clone()))
         .expect("Failed to query storage.")
 }
 