Arm-Kaz/Aptos7

[dag] basic dag fetcher
Add skeleton for dag fetcher that sends a bitmask of local dag and target node to remote and receives missing nodes back.

consensus/src/dag/dag_fetcher.rs
@@ -1,0 +1,132 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::dag::{
+    dag_store::Dag,
+    types::{CertifiedNode, DAGMessage, DAGNetworkSender, Node, NodeMetadata},
+};
+use aptos_consensus_types::common::{Author, Round};
+use aptos_infallible::RwLock;
+use aptos_logger::error;
+use aptos_types::{epoch_state::EpochState, validator_verifier::ValidatorVerifier};
+use serde::{Deserialize, Serialize};
+use std::{sync::Arc, time::Duration};
+use tokio::sync::{
+    mpsc::{Receiver, Sender},
+    oneshot,
+};
+
+/// Represents a request to fetch missing dependencies for `target`, `start_round` represents
+/// the first round we care about in the DAG, `exists_bitmask` is a two dimensional bitmask represents
+/// if a node exist at [start_round + index][validator_index].
+#[derive(Serialize, Deserialize, Clone)]
+struct FetchRequest {
+    target: NodeMetadata,
+    start_round: Round,
+    exists_bitmask: Vec<Vec<bool>>,
+}
+
+/// Represents a response to FetchRequest, `certified_nodes` are indexed by [round][validator_index]
+/// It should fill in gaps from the `exists_bitmask` according to the parents from the `target_digest` node.
+#[derive(Serialize, Deserialize, Clone)]
+struct FetchResponse {
+    epoch: u64,
+    certifies_nodes: Vec<Vec<CertifiedNode>>,
+}
+
+impl FetchResponse {
+    pub fn verify(
+        self,
+        _request: &FetchRequest,
+        _validator_verifier: &ValidatorVerifier,
+    ) -> anyhow::Result<Self> {
+        todo!("verification");
+    }
+}
+
+impl DAGMessage for FetchRequest {
+    fn epoch(&self) -> u64 {
+        self.target.epoch()
+    }
+}
+
+impl DAGMessage for FetchResponse {
+    fn epoch(&self) -> u64 {
+        self.epoch
+    }
+}
+
+enum FetchCallback {
+    Node(Node, oneshot::Sender<Node>),
+    CertifiedNode(CertifiedNode, oneshot::Sender<CertifiedNode>),
+}
+
+impl FetchCallback {
+    pub fn responders(&self, validators: &[Author]) -> Vec<Author> {
+        match self {
+            FetchCallback::Node(node, _) => vec![*node.author()],
+            FetchCallback::CertifiedNode(node, _) => node.certificate().signers(validators),
+        }
+    }
+
+    pub fn notify(self) {
+        if let Err(_) = match self {
+            FetchCallback::Node(node, sender) => sender.send(node).map_err(|_| ()),
+            FetchCallback::CertifiedNode(node, sender) => sender.send(node).map_err(|_| ()),
+        } {
+            error!("Failed to send node back");
+        }
+    }
+}
+
+struct DagFetcher {
+    epoch_state: Arc<EpochState>,
+    network: Arc<dyn DAGNetworkSender>,
+    dag: Arc<RwLock<Dag>>,
+    request_rx: Receiver<(FetchRequest, FetchCallback)>,
+}
+
+impl DagFetcher {
+    pub fn new(
+        epoch_state: Arc<EpochState>,
+        network: Arc<dyn DAGNetworkSender>,
+        dag: Arc<RwLock<Dag>>,
+    ) -> (Self, Sender<(FetchRequest, FetchCallback)>) {
+        let (request_tx, request_rx) = tokio::sync::mpsc::channel(16);
+        (
+            Self {
+                epoch_state,
+                network,
+                dag,
+                request_rx,
+            },
+            request_tx,
+        )
+    }
+
+    pub async fn start(mut self) {
+        while let Some((request, callback)) = self.request_rx.recv().await {
+            let responders =
+                callback.responders(&self.epoch_state.verifier.get_ordered_account_addresses());
+            let network_request = request.clone().into_network_message();
+            if let Ok(response) = self
+                .network
+                .send_rpc_with_retry(responders, network_request, Duration::from_secs(1))
+                .await
+                .and_then(FetchResponse::from_network_message)
+                .and_then(|response| response.verify(&request, &self.epoch_state.verifier))
+            {
+                // TODO: support chunk response or fallback to state sync
+                let mut dag_writer = self.dag.write();
+                for rounds in response.certifies_nodes {
+                    for node in rounds {
+                        if let Err(e) = dag_writer.add_node(node) {
+                            error!("Failed to add node {}", e);
+                        }
+                    }
+                }
+                callback.notify();
+            }
+        }
+    }
+}

consensus/src/dag/tests/reliable_broadcast_tests.rs
@@ -3,8 +3,8 @@
 
 use crate::{
     dag::{
-        reliable_broadcast::{BroadcastStatus, DAGNetworkSender, ReliableBroadcast},
-        types::DAGMessage,
+        reliable_broadcast::{BroadcastStatus, ReliableBroadcast},
+        types::{DAGMessage, DAGNetworkSender},
     },
     network_interface::ConsensusMsg,
 };
@@ -100,6 +100,15 @@ impl DAGNetworkSender for TestDAGSender {
             .lock()
             .insert(receiver, TestMessage::from_network_message(message)?);
         Ok(TestAck.into_network_message())
+    }
+
+    async fn send_rpc_with_retry(
+        &self,
+        _responders: Vec<Author>,
+        _message: ConsensusMsg,
+        _timeout: Duration,
+    ) -> anyhow::Result<ConsensusMsg> {
+        unimplemented!();
     }
 }
 

consensus/src/dag/types.rs
@@ -14,8 +14,9 @@ use aptos_types::{
     aggregate_signature::{AggregateSignature, PartialSignatures},
     epoch_state::EpochState,
 };
+use async_trait::async_trait;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use std::{collections::HashSet, ops::Deref, sync::Arc};
+use std::{collections::HashSet, ops::Deref, sync::Arc, time::Duration};
 
 pub trait DAGMessage: Sized + Clone + Serialize + DeserializeOwned {
     fn epoch(&self) -> u64;
@@ -33,6 +34,23 @@ pub trait DAGMessage: Sized + Clone + Serialize + DeserializeOwned {
             data: bcs::to_bytes(&self).unwrap(),
         })
     }
+}
+
+#[async_trait]
+pub trait DAGNetworkSender: Send + Sync {
+    async fn send_rpc(
+        &self,
+        receiver: Author,
+        message: ConsensusMsg,
+        timeout: Duration,
+    ) -> anyhow::Result<ConsensusMsg>;
+
+    async fn send_rpc_with_retry(
+        &self,
+        responders: Vec<Author>,
+        message: ConsensusMsg,
+        timeout: Duration,
+    ) -> anyhow::Result<ConsensusMsg>;
 }
 
 /// Represents the metadata about the node, without payload and parents from Node
@@ -56,6 +74,10 @@ impl NodeMetadata {
 
     pub fn author(&self) -> &Author {
         &self.author
+    }
+
+    pub fn epoch(&self) -> u64 {
+        self.epoch
     }
 }
 
@@ -143,6 +165,10 @@ impl Node {
     pub fn parents(&self) -> &[NodeMetadata] {
         &self.parents
     }
+
+    pub fn author(&self) -> &Author {
+        self.metadata.author()
+    }
 }
 
 /// Quorum signatures over the node digest
@@ -161,9 +187,13 @@ impl NodeCertificate {
             signatures,
         }
     }
+
+    pub fn signers(&self, validators: &[Author]) -> Vec<Author> {
+        self.signatures.get_signers_addresses(validators)
+    }
 }
 
-#[derive(Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct CertifiedNode {
     node: Node,
     certificate: NodeCertificate,
@@ -172,6 +202,10 @@ pub struct CertifiedNode {
 impl CertifiedNode {
     pub fn new(node: Node, certificate: NodeCertificate) -> Self {
         Self { node, certificate }
+    }
+
+    pub fn certificate(&self) -> &NodeCertificate {
+        &self.certificate
     }
 }
 

vhqtvn/revela

consensus/src/dag/dag_fetcher.rs
@@ -1,0 +1,134 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::dag::{
+    dag_store::Dag,
+    types::{CertifiedNode, DAGMessage, DAGNetworkSender, Node, NodeMetadata},
+};
+use aptos_consensus_types::common::{Author, Round};
+use aptos_infallible::RwLock;
+use aptos_logger::error;
+use aptos_types::{epoch_state::EpochState, validator_verifier::ValidatorVerifier};
+use serde::{Deserialize, Serialize};
+use std::{sync::Arc, time::Duration};
+use tokio::sync::{
+    mpsc::{Receiver, Sender},
+    oneshot,
+};
+
+/// Represents a request to fetch missing dependencies for `target`, `start_round` represents
+/// the first round we care about in the DAG, `exists_bitmask` is a two dimensional bitmask represents
+/// if a node exist at [start_round + index][validator_index].
+#[derive(Serialize, Deserialize, Clone)]
+struct FetchRequest {
+    target: NodeMetadata,
+    start_round: Round,
+    exists_bitmask: Vec<Vec<bool>>,
+}
+
+/// Represents a response to FetchRequest, `certified_nodes` are indexed by [round][validator_index]
+/// It should fill in gaps from the `exists_bitmask` according to the parents from the `target_digest` node.
+#[derive(Serialize, Deserialize, Clone)]
+struct FetchResponse {
+    epoch: u64,
+    certifies_nodes: Vec<Vec<CertifiedNode>>,
+}
+
+impl FetchResponse {
+    pub fn verify(
+        self,
+        _request: &FetchRequest,
+        _validator_verifier: &ValidatorVerifier,
+    ) -> anyhow::Result<Self> {
+        todo!("verification");
+    }
+}
+
+impl DAGMessage for FetchRequest {
+    fn epoch(&self) -> u64 {
+        self.target.epoch()
+    }
+}
+
+impl DAGMessage for FetchResponse {
+    fn epoch(&self) -> u64 {
+        self.epoch
+    }
+}
+
+enum FetchCallback {
+    Node(Node, oneshot::Sender<Node>),
+    CertifiedNode(CertifiedNode, oneshot::Sender<CertifiedNode>),
+}
+
+impl FetchCallback {
+    pub fn responders(&self, validators: &[Author]) -> Vec<Author> {
+        match self {
+            FetchCallback::Node(node, _) => vec![*node.author()],
+            FetchCallback::CertifiedNode(node, _) => node.certificate().signers(validators),
+        }
+    }
+
+    pub fn notify(self) {
+        if match self {
+            FetchCallback::Node(node, sender) => sender.send(node).map_err(|_| ()),
+            FetchCallback::CertifiedNode(node, sender) => sender.send(node).map_err(|_| ()),
+        }
+        .is_err()
+        {
+            error!("Failed to send node back");
+        }
+    }
+}
+
+struct DagFetcher {
+    epoch_state: Arc<EpochState>,
+    network: Arc<dyn DAGNetworkSender>,
+    dag: Arc<RwLock<Dag>>,
+    request_rx: Receiver<(FetchRequest, FetchCallback)>,
+}
+
+impl DagFetcher {
+    pub fn new(
+        epoch_state: Arc<EpochState>,
+        network: Arc<dyn DAGNetworkSender>,
+        dag: Arc<RwLock<Dag>>,
+    ) -> (Self, Sender<(FetchRequest, FetchCallback)>) {
+        let (request_tx, request_rx) = tokio::sync::mpsc::channel(16);
+        (
+            Self {
+                epoch_state,
+                network,
+                dag,
+                request_rx,
+            },
+            request_tx,
+        )
+    }
+
+    pub async fn start(mut self) {
+        while let Some((request, callback)) = self.request_rx.recv().await {
+            let responders =
+                callback.responders(&self.epoch_state.verifier.get_ordered_account_addresses());
+            let network_request = request.clone().into_network_message();
+            if let Ok(response) = self
+                .network
+                .send_rpc_with_fallbacks(responders, network_request, Duration::from_secs(1))
+                .await
+                .and_then(FetchResponse::from_network_message)
+                .and_then(|response| response.verify(&request, &self.epoch_state.verifier))
+            {
+                // TODO: support chunk response or fallback to state sync
+                let mut dag_writer = self.dag.write();
+                for rounds in response.certifies_nodes {
+                    for node in rounds {
+                        if let Err(e) = dag_writer.add_node(node) {
+                            error!("Failed to add node {}", e);
+                        }
+                    }
+                }
+                callback.notify();
+            }
+        }
+    }
+}

consensus/src/dag/tests/reliable_broadcast_tests.rs
@@ -3,8 +3,8 @@
 
 use crate::{
     dag::{
-        reliable_broadcast::{BroadcastStatus, DAGNetworkSender, ReliableBroadcast},
-        types::DAGMessage,
+        reliable_broadcast::{BroadcastStatus, ReliableBroadcast},
+        types::{DAGMessage, DAGNetworkSender},
     },
     network_interface::ConsensusMsg,
 };
@@ -100,6 +100,15 @@ impl DAGNetworkSender for TestDAGSender {
             .lock()
             .insert(receiver, TestMessage::from_network_message(message)?);
         Ok(TestAck.into_network_message())
+    }
+
+    async fn send_rpc_with_fallbacks(
+        &self,
+        _responders: Vec<Author>,
+        _message: ConsensusMsg,
+        _timeout: Duration,
+    ) -> anyhow::Result<ConsensusMsg> {
+        unimplemented!();
     }
 }
 

consensus/src/dag/types.rs
@@ -14,8 +14,9 @@ use aptos_types::{
     aggregate_signature::{AggregateSignature, PartialSignatures},
     epoch_state::EpochState,
 };
+use async_trait::async_trait;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use std::{collections::HashSet, ops::Deref, sync::Arc};
+use std::{collections::HashSet, ops::Deref, sync::Arc, time::Duration};
 
 pub trait DAGMessage: Sized + Clone + Serialize + DeserializeOwned {
     fn epoch(&self) -> u64;
@@ -33,6 +34,25 @@ pub trait DAGMessage: Sized + Clone + Serialize + DeserializeOwned {
             data: bcs::to_bytes(&self).unwrap(),
         })
     }
+}
+
+#[async_trait]
+pub trait DAGNetworkSender: Send + Sync {
+    async fn send_rpc(
+        &self,
+        receiver: Author,
+        message: ConsensusMsg,
+        timeout: Duration,
+    ) -> anyhow::Result<ConsensusMsg>;
+
+    /// Given a list of potential responders, sending rpc to get response from any of them and could
+    /// fallback to more in case of failures.
+    async fn send_rpc_with_fallbacks(
+        &self,
+        responders: Vec<Author>,
+        message: ConsensusMsg,
+        timeout: Duration,
+    ) -> anyhow::Result<ConsensusMsg>;
 }
 
 /// Represents the metadata about the node, without payload and parents from Node
@@ -56,6 +76,10 @@ impl NodeMetadata {
 
     pub fn author(&self) -> &Author {
         &self.author
+    }
+
+    pub fn epoch(&self) -> u64 {
+        self.epoch
     }
 }
 
@@ -143,6 +167,10 @@ impl Node {
     pub fn parents(&self) -> &[NodeMetadata] {
         &self.parents
     }
+
+    pub fn author(&self) -> &Author {
+        self.metadata.author()
+    }
 }
 
 /// Quorum signatures over the node digest
@@ -161,9 +189,13 @@ impl NodeCertificate {
             signatures,
         }
     }
+
+    pub fn signers(&self, validators: &[Author]) -> Vec<Author> {
+        self.signatures.get_signers_addresses(validators)
+    }
 }
 
-#[derive(Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct CertifiedNode {
     node: Node,
     certificate: NodeCertificate,
@@ -172,6 +204,10 @@ pub struct CertifiedNode {
 impl CertifiedNode {
     pub fn new(node: Node, certificate: NodeCertificate) -> Self {
         Self { node, certificate }
+    }
+
+    pub fn certificate(&self) -> &NodeCertificate {
+        &self.certificate
     }
 }
 