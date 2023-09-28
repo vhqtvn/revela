fEst1ck/aptos-core

[crypto] add secp256k1 support to aptos-crypto
Pretty straight forward, but I think our crypto apis are really not
great. The amount of traits that should really be compressed down into a
single trait for PublicKey, PrivateKey, and Signature. This would make
maintaining and adding new libraries so much easier.

crates/aptos-crypto/src/lib.rs
@@ -15,6 +15,7 @@ pub mod hash;
 pub mod hkdf;
 pub mod multi_ed25519;
 pub mod noise;
+pub mod secp256k1;
 pub mod test_utils;
 pub mod traits;
 pub mod validatable;

crates/aptos-crypto/src/traits.rs
@@ -305,7 +305,11 @@ pub trait Genesis: PrivateKey {
 pub(crate) mod private {
     pub trait Sealed {}
 
-    // Implement for the ed25519, multi-ed25519 signatures
+    impl Sealed for crate::bls12381::PrivateKey {}
+    impl Sealed for crate::bls12381::PublicKey {}
+    impl Sealed for crate::bls12381::Signature {}
+    impl Sealed for crate::bls12381::ProofOfPossession {}
+
     impl Sealed for crate::ed25519::Ed25519PrivateKey {}
     impl Sealed for crate::ed25519::Ed25519PublicKey {}
     impl Sealed for crate::ed25519::Ed25519Signature {}
@@ -314,8 +318,7 @@ pub(crate) mod private {
     impl Sealed for crate::multi_ed25519::MultiEd25519PublicKey {}
     impl Sealed for crate::multi_ed25519::MultiEd25519Signature {}
 
-    impl Sealed for crate::bls12381::PrivateKey {}
-    impl Sealed for crate::bls12381::PublicKey {}
-    impl Sealed for crate::bls12381::Signature {}
-    impl Sealed for crate::bls12381::ProofOfPossession {}
+    impl Sealed for crate::secp256k1::PrivateKey {}
+    impl Sealed for crate::secp256k1::PublicKey {}
+    impl Sealed for crate::secp256k1::Signature {}
 }

vhqtvn/revela

crates/aptos-crypto/src/lib.rs
@@ -15,6 +15,7 @@ pub mod hash;
 pub mod hkdf;
 pub mod multi_ed25519;
 pub mod noise;
+pub mod secp256k1_ecdsa;
 pub mod test_utils;
 pub mod traits;
 pub mod validatable;

crates/aptos-crypto/src/traits.rs
@@ -305,7 +305,11 @@ pub trait Genesis: PrivateKey {
 pub(crate) mod private {
     pub trait Sealed {}
 
-    // Implement for the ed25519, multi-ed25519 signatures
+    impl Sealed for crate::bls12381::PrivateKey {}
+    impl Sealed for crate::bls12381::PublicKey {}
+    impl Sealed for crate::bls12381::Signature {}
+    impl Sealed for crate::bls12381::ProofOfPossession {}
+
     impl Sealed for crate::ed25519::Ed25519PrivateKey {}
     impl Sealed for crate::ed25519::Ed25519PublicKey {}
     impl Sealed for crate::ed25519::Ed25519Signature {}
@@ -314,8 +318,7 @@ pub(crate) mod private {
     impl Sealed for crate::multi_ed25519::MultiEd25519PublicKey {}
     impl Sealed for crate::multi_ed25519::MultiEd25519Signature {}
 
-    impl Sealed for crate::bls12381::PrivateKey {}
-    impl Sealed for crate::bls12381::PublicKey {}
-    impl Sealed for crate::bls12381::Signature {}
-    impl Sealed for crate::bls12381::ProofOfPossession {}
+    impl Sealed for crate::secp256k1_ecdsa::PrivateKey {}
+    impl Sealed for crate::secp256k1_ecdsa::PublicKey {}
+    impl Sealed for crate::secp256k1_ecdsa::Signature {}
 }